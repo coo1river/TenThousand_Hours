55fa3ccac4a466344c6e2c199c072223baafbcc6
coolriver <longpaca@gmail.com>
2023-09-17 05:49:21 +0900

    헤더 정렬, 인용구 따옴표 추가/정렬

--- src/components/header/header.styled.js
+++ src/components/header/header.styled.js
@@ -1,23 +1,62 @@
 import styled from "styled-components";
+import before from "../../image/before.png";
+import after from "../../image/after.png";
 
 export const QuoteFirst = styled.strong`
+  margin-top: 66px;
+  margin-bottom: 78px;
+
   font-family: "OTEnjoystoriesBA";
   font-size: 36px;
   color: yellow;
 `;
 
 export const QuoteSecond = styled.div`
+  display: flex;
+  gap: 14px;
+
+  text-align: center;
+
   font-family: "GmarketSansMedium";
-  font-size: 24px;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: normal;
+
+  &::before,
+  &::after {
+    content: "";
+    display: inline-block;
+    background-repeat: no-repeat;
+
+    width: 46px;
+    height: 46px;
+    margin-top: 13px;
+    margin-bottom: 50px;
+  }
+
+  &::before {
+    background-image: url(${before});
+  }
+
+  &::after {
+    background-image: url(${after});
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
 `;
 
 export const QuoteStrong = styled.strong`
-  font-weight: bold;
+  font-size: 24px;
+  font-weight: 700;
 `;
 
 export const HeaderStyle = styled.div`
-  padding-top: 193px;
   display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
 `;
 
@@ -26,12 +65,11 @@ export const LogoImg = styled.img`
   height: 112px;
 
   position: absolute;
+  top: 200px;
 `;
 
-export const BgImg = styled.img`
+export const ClockImg = styled.img`
   width: 221px;
   height: 225px;
-
-  position: absolute;
-  top: 138px;
+  margin-top: 140px;
 `;
