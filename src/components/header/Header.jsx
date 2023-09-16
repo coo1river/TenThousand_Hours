1a7456b7cb2f538e0ae779a737151c45a8d3263f
coolriver <longpaca@gmail.com>
2023-09-16 23:35:17 +0900

    헤더 인용구 추가

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,9 +5,16 @@ import { BgImg, LogoImg, HeaderStyle } from "./header.styled";
 
 export default function Header() {
   return (
-    <HeaderStyle>
-      <BgImg src={Bg} />
-      <LogoImg src={Logo} alt="" />
-    </HeaderStyle>
+    <>
+      <HeaderStyle>
+        <BgImg src={Bg} />
+        <LogoImg src={Logo} alt="" />
+        <HeaderQuote />
+      </HeaderStyle>
+    </>
   );
+}
+
+function HeaderQuote() {
+  return <p>"연습은 어제의 당신보다 당신을 더 낫게 만든다."</p>;
 }
