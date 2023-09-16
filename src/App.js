fea41563c346cffed053c0b4f3f1207cf0f4b5ee
coolriver <longpaca@gmail.com>
2023-09-17 04:31:25 +0900

    폰트 추가, 헤더 - 인용구 추가

--- src/App.js
+++ src/App.js
@@ -7,7 +7,22 @@ ${reset}
 body{
   background-color: #5b2386;
   color: white;
+
+  @font-face {
+    font-family: 'OTEnjoystoriesBA';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_two@1.0/OTEnjoystoriesBA.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
 }
+
+@font-face {
+    font-family: 'GmarketSansMedium';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+}
+
 `;
 
 function App() {
--- src/components/header/header.styled.js
+++ src/components/header/header.styled.js
@@ -1,5 +1,20 @@
 import styled from "styled-components";
 
+export const QuoteFirst = styled.strong`
+  font-family: "OTEnjoystoriesBA";
+  font-size: 36px;
+  color: yellow;
+`;
+
+export const QuoteSecond = styled.div`
+  font-family: "GmarketSansMedium";
+  font-size: 24px;
+`;
+
+export const QuoteStrong = styled.strong`
+  font-weight: bold;
+`;
+
 export const HeaderStyle = styled.div`
   padding-top: 193px;
   display: flex;
