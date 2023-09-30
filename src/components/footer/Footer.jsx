d39f328f10f1d8673aa5640afba54bff965e21e7
coolriver <longpaca@gmail.com>
2023-09-30 23:52:39 +0900

    footer 수정

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import LogoImg from "../../image/Logo.svg";
+import LogoImg from "../../image/위니브.svg";
+import { FooterCtn } from "./footer.styled";
 
 export default function Footer() {
   return (
-    <div>
+    <FooterCtn>
       <img src={LogoImg} alt="위니브 로고" />
       <p>
         ※ 본 서비스 내 이미지 및 콘텐츠의 저작권은 주식회사 WeNiv에 있습니다.
-        </p>
-        <p>
-        수정 및 재배포, 무단 도용 시 법적인 문제가 발생할 수 있습니다.
       </p>
-    </div>
+      <p>수정 및 재배포, 무단 도용 시 법적인 문제가 발생할 수 있습니다.</p>
+    </FooterCtn>
   );
 }
--- src/components/footer/footer.styled.js
+++ src/components/footer/footer.styled.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 
-export const FooterCtn = styled.p`
+export const FooterCtn = styled.div`
+  margin-top: 130px;
   font-family: "Noto Sans KR";
+  font-size: 12px;
+  text-align: center;
 `;
