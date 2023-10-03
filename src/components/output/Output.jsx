d46042777d64f2b8044bb5b7bb5565fdfa2c2228
coolriver <coolnriver@gmail.com>
2023-10-04 00:08:12 +0900

    output 내용 추가

--- src/components/output/Output.jsx
+++ src/components/output/Output.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Output() {
-  return <div></div>;
+  return (
+    <div>
+      <p>
+        당신은 프로그래밍 전문가가 되기 위해서
+        <br />
+        대략 5110일 이상 훈련하셔야 합니다! :)
+      </p>
+    </div>
+  );
 }
---
+++ src/components/output/output.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const OutputCtn = styled.p`
+  font-family: "gmarketMedium";
+  font-size: 24px;
+  line-height: normal;
+`;
