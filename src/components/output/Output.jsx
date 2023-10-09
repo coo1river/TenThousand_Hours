8c92339f50c4c4769fb10f249fc7f9a60b2031bb
coo1river <longpaca@gmail.com>
2023-10-09 21:14:52 +0900

    output 버튼 추가

--- src/components/output/Output.jsx
+++ src/components/output/Output.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 
 export default function Output() {
-<<<<<<< HEAD
   return (
     <div>
       <p>
         당신은 프로그래밍 전문가가 되기 위해서
         <br />
         대략 5110일 이상 훈련하셔야 합니다! :)
+        <button>훈련하러 가기 GO!GO!</button>
+        <button>공유하기</button>
       </p>
     </div>
   );
-=======
-  return <div>output</div>;
->>>>>>> e1ad05c28e6679cd019301bc59b64aa8c0c581d5
 }
