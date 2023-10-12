f0004d85c8342d20f55d13cd48930efd98513902
coo1river <longpaca@gmail.com>
2023-10-12 22:28:19 +0900

    훈련하기 버튼 스타일

--- src/components/output/Output.jsx
+++ src/components/output/Output.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { BtnOutput, OutputCtn } from "./output.styled";
 
 export default function Output() {
   return (
     <div>
-      <p>
+      <OutputCtn>
         당신은 프로그래밍 전문가가 되기 위해서
         <br />
         대략 5110일 이상 훈련하셔야 합니다! :)
-        <button>훈련하러 가기 GO!GO!</button>
-        <button>공유하기</button>
-      </p>
+      </OutputCtn>
+      <BtnOutput>훈련하러 가기 GO!GO!</BtnOutput>
+      <button>공유하기</button>
     </div>
   );
 }
--- src/components/output/output.styled.js
+++ src/components/output/output.styled.js
@@ -3,5 +3,25 @@ import styled from "styled-components";
 export const OutputCtn = styled.p`
   font-family: "gmarketMedium";
   font-size: 24px;
+  text-align: center;
   line-height: normal;
+`;
+
+export const BtnOutput = styled.button`
+  background-color: #fcee21;
+  color: #5b2386;
+  margin-top: 70px;
+  border-radius: 30px;
+  padding: 10px 40px;
+
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
+
+  font-family: "gmarketBold";
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
